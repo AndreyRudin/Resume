41fccb5d2110758e3ffd93dafa253718f6d7d2e7
Andrey <rudin1996@mail.ru>
2020-03-31 17:21:32 +0300

    Theory JS 1

--- js/script.js
+++ js/script.js
@@ -7,3 +7,5 @@ while (count <= 10){
 // console.log(sum(range(1,10)));
 // console.log("первая \nвторая"*2);
 console.log(typeof `значение ${100/2}`); //!литерал шаблона(записанные в нем операции высичляются а затем преобразуются в текст)
+console.log(true && false);
+console.log(true || false); //!логические операции
