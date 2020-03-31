863d844b92e44916a3687c0c2a199e53b301ccd9
Andrey <rudin1996@mail.ru>
2020-03-31 16:28:50 +0300

    Initial resume

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// import Math;
+let total=0, count=1;
+while (count <= 10){
+    total +=count;
+    count+=1;
+}
+// console.log(sum(range(1,10)));
+// console.log("первая \nвторая"*2);
+console.log(typeof `значение ${100/2}`); //!литерал шаблона(записанные в нем операции высичляются а затем преобразуются в текст)
