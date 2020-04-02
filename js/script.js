bbae551a9d852a8198a996c1db7b43602181793e
Andrey <rudin1996@mail.ru>
2020-04-02 13:10:54 +0300

    New

--- js/script.js
+++ js/script.js
@@ -17,27 +17,157 @@
 // console.log(one+two);
 //* побочные эффект - когда что-то меняется внутри машины.
 //*функции Number.isNaN(theNumber)-проверяет является ли аругмент NaN, любая строка это NaN
-console.log(Math.max(2,4));
+// console.log(Math.max(2,4));
 //*Условное выполнение, т.е. выполнение программы с использованием условия, цикл do выполняется хотя бы 1 раз
-let theNumber = Number("15"), index = 0;
-while (index < 2) {
-  if (!Number.isNaN(theNumber) && theNumber < 10) {
-    console.log("Это число является квадратным корнем из "+ theNumber*theNumber);
-  }
-  else if (!Number.isNaN(theNumber) && theNumber >= 10) {
-    console.log("Это число является квадратным корнем из "+ theNumber*theNumber +" и оно больше 10");
-  }
-  else{
-    console.log("это не число");
-  }
-  index += 1;
-}
+// let theNumber = Number("15"), index = 0;
+// while (index < 2) {
+//   if (!Number.isNaN(theNumber) && theNumber < 10) {
+//     console.log("Это число является квадратным корнем из "+ theNumber*theNumber);
+//   }
+//   else if (!Number.isNaN(theNumber) && theNumber >= 10) {
+//     console.log("Это число является квадратным корнем из "+ theNumber*theNumber +" и оно больше 10");
+//   }
+//   else{
+//     console.log("это не число");
+//   }
+//   index += 1;
+// }
 
-do {
+/*do {
   console.log(1);
   index += 1;
-} while (index < 3);
+} while (index < 3);*/
 
-for (let number = 0; number < 12; number += 2) {
-  console.log(number);
-}
+// for (let number = 0; number < 12; number += 2) {
+//   console.log(number);
+//   break;
+// }
+
+// for (let number = 1; number < 100; number += 1){
+//   if (number % 3 === 0 &&number % 5 === 0) {
+//     console.log("FizzBuzz");
+//   }
+//   else if (number % 3 === 0){
+//     console.log("Fizz");
+//   }
+//   else if (number % 5 === 0){
+//     console.log("Buzz");
+//   }
+//   else{
+//     console.log(number);
+//   }
+// }
+
+// Шахматное поле
+// let size = 8, flag = true,result = "";
+// for (let index1 = 0; index1 < size; index1 ++) {
+//   for (let index2 = 0; index2 < size; index2 ++) {
+//     if (flag) {
+//       result += "#"
+//     }
+//     else{
+//       result += " "
+//     }
+//     flag = !flag
+//   }
+// result += "\n"
+// }
+// console.log(result)
+
+// Функции
+// const makeNoise = function(){
+//   console.log("Эгегей");
+// };
+
+// makeNoise();
+
+// const power = function(base, exponent){
+//   let result = 1;
+//   for (let count = 0; count < exponent; count ++){
+//     result *= base;
+//   }
+//   return result;
+// }
+
+// console.log(power(2,11))
+
+// Область видимости, локальные и глобальные переменные
+// let x = 10;
+// if (true){
+//   var y = 5;
+//   let z = 10;
+//   console.log(x+y+z);
+// };
+//  console.log(x+y); //если попытаться посчитать сумму с z то будет ошибка, так как let и const задают переменную только в блоке в котором ее создали
+
+// функцию можно создать также, через ее объявление
+// function future() {
+//   return "летающих машин не будет"
+// };
+
+// const power = (base, exponent) =>{
+//   let result = 1;
+//   for (let count = 0; count < exponent; count ++){
+//     result *= base;
+//   }
+//   return result;
+// }
+
+// console.log(power(10,4));
+
+// Упражнения функции минимум
+// const min = function(a,b){
+//   result = a;
+//   if (b < a){
+//     result = b;
+//   };
+//   console.log(result);
+// };
+// function min2(a,b){
+//   result = a;
+//   if (b < a){
+//     result = b;
+//   };
+//   console.log(result);
+// };
+// const min3 = (a,b) => {
+//   result = a;
+//   if (b < a){
+//     result = b;
+//   };
+//   console.log(result);
+// };
+// min3(1,2);
+
+// Количество букв в слове
+// function findNumberSymbolsInWord(word, symbol="В") {
+//   let count = 0;
+//   for (let i = 0; i <= word.length-1; i++) {
+//     if (word[i] == symbol){
+//       count ++;
+//     };
+//   };
+//   console.log(count);
+// };
+// findNumberSymbolsInWord("ВеаСВаВааааВ", "а");
+
+// Упраженение рекурсия
+// function isEven(number){
+//   if (number == 1){
+//     console.log("нечетное");
+//   };
+//   {
+
+//   }
+// }
+
+// Описание объект
+let kiska = {
+  name: "ksusha",
+  status: ["plusha","hrusha","pushistik"],
+  age: "25",
+  "ела сегодня картошку": "нет"
+};
+kiska["Пусит Андрея"] = false;
+
+console.log(kiska["Пусит Андрея"]);
